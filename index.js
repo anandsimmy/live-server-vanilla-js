224ca7c6f647cdb1242c563597f8b33a87f0c799
Anand Simmy <anandsimmy7@gmail.com>
2021-12-06 08:11:32 +0530

    circle rotation changes

--- index.js
+++ index.js
@@ -1,72 +1,8 @@
-let a = {
-  name: 'Jon',
-  fname: {
-    title: 'Got',
-    hello: [3, 4, { 1: 2 }],
-  },
-  arr: [1, 2, {}, [5, 6]],
-};
+const outerCircle = documetnt.getElementById('');
+const innerCircle = documetnt.getElementById('inner-circle');
 
-//deepclone implementation
-const deepClone = (param) => {
-  if (Array.isArray(param)) {
-    const newArr = [];
-    param.forEach((item, index) => {
-      if (Array.isArray(item) || typeof item === 'object') {
-        newArr[index] = deepClone(item);
-      } else {
-        newArr[index] = item;
-      }
-    });
-    return newArr;
-  } else if (typeof param === 'object') {
-    const newObj = {};
-    Object.keys(param).forEach((key) => {
-      if (Array.isArray(param[key]) || typeof param[key] === 'object') {
-        newObj[key] = deepClone(param[key]);
-      } else {
-        newObj[key] = param[key];
-      }
-    });
-    return newObj;
-  }
-};
-
-let b = deepClone(a);
-
-const person = {
-  firstName: '',
-  lastName: '',
-  setName(name) {
-    const self = this;
-    function splitName(n) {
-      self.firstName = n.split(' ')[0];
-      self.lastName = n.split(' ')[1];
-    }
-    splitName(name);
-  },
-};
-
-person.setName('john doe');
-
-function Counter() {
-  this.increment = function () {
-    count++;
-    console.log(count);
-  };
-  this.decrement = function () {
-    count--;
-    console.log(count);
-  };
+function rotateInner() {
+  const { top, left } = innerCircle.getBoundingClientRect();
 }
 
-const c = new Counter();
-const d = new Counter();
-
-console.log(c);
-c.increment();
-c.increment();
-c.increment();
-c.decrement();
-c.increment();
-d.increment();
+setInterval(() => rotateInner(), 100);
